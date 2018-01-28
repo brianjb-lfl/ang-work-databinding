1afd3782418b2f02262469a91204985ec591b2c7
Brian Besaw <bjbesaw@outlook.com>
2018-01-28 11:24:44 -0500

    Added custom data and event binding.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
+
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
 
   constructor() { }
 
@@ -13,20 +18,20 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
     });
+
+    this.newServerName = '';
+    this.newServerContent = '';
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.blueprintCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
     });
   }
-
 
 }
